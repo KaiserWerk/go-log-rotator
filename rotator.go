2c0339819c4e96c13eccc5d61c8bd612859ea3c4
Robin Kaiser <web@r-k.mx>
2022-01-16 17:22:03 +0100

    initially required files

---
+++ rotator.go
@@ -0,0 +1,158 @@
+package rotator
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+)
+
+const (
+	timeFormat = "2006-01-02T15-04-05"
+)
+
+// Rotator represents a struct responsible for writing into a log file while
+// rotating the file when it reached maxSize.
+type Rotator struct {
+	path        string
+	filename    string
+	currentSize uint64
+	maxSize     uint64
+	filesToKeep uint8
+	writer      *os.File
+	permissions os.FileMode
+}
+
+// New returns a new rotator prepared to be written to.
+//
+// path: the path where log files should be written to, e.g. "/var/logs/myapp" or `C:\Logs`
+//
+// filename: the name the log files are supposed to have, without a suffix (".log" will be added automatically)
+//
+// maxSize: the maximum size in bytes a created log file may reach before it is rotated, e.g. 10 << 20 for 10MB
+//
+// perms: the file permissions in octal notation, e.g. 0744 (not relevant for windows)
+//
+// filesToKeep: the number of rotated files to keep. The currently written file is not counted towards this limit
+func New(path, filename string, maxSize uint64, perms fs.FileMode, filesToKeep uint8) (*Rotator, error) {
+	r := Rotator{
+		path:        path,
+		filename:    filename,
+		maxSize:     maxSize,
+		permissions: perms,
+		filesToKeep: filesToKeep,
+	}
+
+	err := os.MkdirAll(r.path, r.permissions)
+	if err != nil {
+		return nil, err
+	}
+
+	if stat, err := os.Stat(filepath.Join(r.path, r.filename)); err != nil && !errors.Is(err, fs.ErrNotExist) {
+		if stat.Size() > int64(r.maxSize) {
+			if r.writer != nil {
+				r.writer.Close()
+			}
+			err = os.Rename(filepath.Join(r.path, r.filename), filepath.Join(r.path, r.determineNextFilename()))
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			r.currentSize = uint64(stat.Size())
+		}
+	}
+
+	fh, err := os.OpenFile(filepath.Join(r.path, r.filename), os.O_APPEND|os.O_RDWR|os.O_CREATE, perms)
+	if err != nil {
+		return nil, err
+	}
+	r.writer = fh
+
+	return &r, nil
+}
+
+// Write writes the data into the log file and initiates rotation, if necessary
+func (r *Rotator) Write(data []byte) (int, error) {
+	if r.currentSize+uint64(len(data)) > r.maxSize {
+		if r.writer != nil {
+			err := r.writer.Close()
+			if err != nil {
+				return 0, err
+			}
+		}
+
+		err := r.removeUnnecessaryFiles()
+		if err != nil {
+			return 0, nil
+		}
+
+		err = os.Rename(filepath.Join(r.path, r.filename), filepath.Join(r.path, r.determineNextFilename()))
+		if err != nil {
+			return 0, err
+		}
+		fh, err := os.OpenFile(filepath.Join(r.path, r.filename), os.O_APPEND|os.O_RDWR|os.O_CREATE, r.permissions)
+		if err != nil {
+			return 0, err
+		}
+		r.writer = fh
+		r.currentSize = 0
+	}
+
+	n, err := r.writer.Write(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return n, err
+}
+
+// determineNextFilename determines the next free filename to be used on rotation
+func (r *Rotator) determineNextFilename() string {
+	return filepath.Join(r.path, fmt.Sprintf("%s.%s", r.filename, time.Now().Format(timeFormat)))
+}
+
+// removeUnnecessaryFiles removes old files and keeps r.filesToKeep files
+func (r *Rotator) removeUnnecessaryFiles() error {
+	files, err := filepath.Glob(filepath.Join(r.path, r.filename) + ".*")
+	if err != nil {
+		return err
+	}
+
+	if len(files) == 0 {
+		return nil
+	}
+
+	sort.Slice(files, func(i, j int) bool {
+		partsI := strings.Split(files[i], ".")
+		partsJ := strings.Split(files[j], ".")
+		dtI, err := time.Parse(timeFormat, partsI[len(partsI)-1])
+		if err != nil {
+			return false
+		}
+		dtJ, err := time.Parse(timeFormat, partsJ[len(partsJ)-1])
+		if err != nil {
+			return false
+		}
+		return dtI.Before(dtJ)
+	})
+
+	filesToRemove := files[:len(files)-int(r.filesToKeep)]
+
+	for _, f := range filesToRemove {
+		err = os.Remove(filepath.Join(r.path, f))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Close closes the io.Writer of the Rotator.
+func (r *Rotator) Close() error {
+	return r.writer.Close()
+}
---
+++ rotator_test.go
@@ -0,0 +1,104 @@
+package rotator
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func Test_newRotatorCreatesAccessibleFile(t *testing.T) {
+	var (
+		testPath     = "testRotator"
+		testFilename = "test.log"
+		testFullPath = filepath.Join(testPath, testFilename)
+		testPerms    = fs.FileMode(0755)
+	)
+	r, err := New(testPath, testFilename, 1, testPerms, 10)
+	if err != nil {
+		t.Errorf("Test_newRotatorCreatesFile | %s failed with error: %s", "newRotator()", err)
+	}
+	err = r.writer.Close()
+	if err != nil {
+		t.Errorf("Test_newRotatorCreatesFile | %s failed with error: %s", "r.Close()", err)
+	}
+	_, err = os.Stat(testFullPath)
+	if err != nil {
+		t.Errorf("Test_newRotatorCreatesFile | %s failed with error: %s", "os.Stat()", err)
+	}
+	err = os.RemoveAll(testPath)
+	if err != nil {
+		t.Errorf("Test_newRotatorCreatesFile | %s failed with error: %s", "os.Remove()", err)
+	}
+}
+
+func Test_newRotatorCanWrite(t *testing.T) {
+	var (
+		testPath     = "testRotator"
+		testFilename = "test.log"
+		testFullPath = filepath.Join(testPath, testFilename)
+		testPerms    = fs.FileMode(0755)
+	)
+	r, err := New(testPath, testFilename, 8, testPerms, 10)
+	if err != nil {
+		t.Errorf("Test_newRotatorCanWrite | %s failed with error: %s", "newRotator()", err)
+	}
+	r.Write([]byte("test"))
+	r.writer.Close()
+	fi, err := os.Stat(testFullPath)
+	if err != nil {
+		t.Errorf("Test_newRotatorCanWrite | %s failed with error: %s", "os.Stat()", err)
+	}
+	if fi.Size() <= 0 {
+		t.Errorf("Test_newRotatorCanWrite | failed - filesize must be bigger than 0, is: %d", fi.Size())
+	}
+	err = os.RemoveAll(testPath)
+	if err != nil {
+		t.Errorf("Test_newRotatorCanWrite | %s failed with error: %s", "os.Remove()", err)
+	}
+}
+
+func Test_RemoveUnnecessaryFiles(t *testing.T) {
+	const (
+		filesToKeep       uint8 = 1
+		expectedFileCount       = int(filesToKeep) + 1
+		filePrefix              = "Test_RemoveUnnecessaryFiles"
+	)
+	rotator, err := New(".", filePrefix+".log", 100, 0600, filesToKeep)
+	if err != nil {
+		t.Fatalf("could not create new rotator: %s", err.Error())
+	}
+
+	for i := 0; i < 15; i++ {
+		_, err = rotator.Write(make([]byte, 120))
+		if err != nil {
+			t.Fatalf("could not write into rotator: %s", err.Error())
+		}
+	}
+
+	err = rotator.Close()
+	if err != nil {
+		t.Fatalf("could not close the rotator: %s", err.Error())
+	}
+
+	err = rotator.removeUnnecessaryFiles()
+	if err != nil {
+		t.Fatalf("could not remove unnecessary file: %s", err.Error())
+	}
+
+	remainingFiles, err := filepath.Glob(filePrefix + "*")
+	if err != nil {
+		t.Fatalf("could not find files with glob: %s", err.Error())
+	}
+
+	if len(remainingFiles) != expectedFileCount {
+		t.Fatalf("expected %d files, but there are %d", expectedFileCount, len(remainingFiles))
+	}
+
+	for _, f := range remainingFiles {
+		err = os.Remove("./" + f)
+		if err != nil {
+			t.Fatalf("could not remove file '%s': %s", f, err.Error())
+		}
+	}
+}
